7b3ab537262fe18271deee903ca8b2f6a208e1ad
LihleLanga <52010581+LihleLanga@users.noreply.github.com>
2022-04-07 14:55:15 +0200

    Commit to 217181147

---
+++ src/main/java/za/ac/cput/entity/Login.java
@@ -0,0 +1,9 @@
+package za.ac.cput.entity;
+
+public class Login {
+    private String Loginid;
+    private String userId;
+    private String userEmail;
+    private String userPassword;
+    
+}
---
+++ src/main/java/za/ac/cput/entity/Payment.java
@@ -0,0 +1,10 @@
+package za.ac.cput.entity;
+
+public class Payment {
+    private String paymentId;
+    private String paymentDate;
+    private String paymentTime;
+    private String paymentTotal;
+    private String paymentDetails;
+
+}
