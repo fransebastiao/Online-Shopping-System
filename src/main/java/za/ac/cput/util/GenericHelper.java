2548d2b8cf3cc0d68081829797aa8b3f19466a45
Edvalter Jamba <edvalterjamba9@gmail.com>
2022-04-08 22:35:10 +0200

    $ git commit -m

--- src/main/java/za/ac/cput/util/GenericHelper.java
+++ src/main/java/za/ac/cput/util/GenericHelper.java
@@ -4,14 +4,5 @@ import java.util.UUID;
 
 public class GenericHelper
 {
-    public static boolean isNullorEmpty(String s)
-    {
-
-        return (s == null || s.isEmpty() || s.equalsIgnoreCase("null"));
-    }
-
-    public static String generateId(){
-
-        return UUID.randomUUID().toString();
-    }
+   
 }
