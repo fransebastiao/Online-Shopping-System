d9a4e8bce9778e60c8ac2527964e056605486cbb
Franciel Sebastião <219466912@mycput.ac.za>
2022-04-08 21:48:22 +0200

    Canges were made

--- src/main/java/za/ac/cput/repository/UserRepository.java
+++ src/main/java/za/ac/cput/repository/UserRepository.java
@@ -1,30 +1,5 @@
 package za.ac.cput.repository;
 
-import java.util.UUID;
-
 public class UserRepository {
 
-    public static boolean isNullorEmpty(String s){
-
-        return (s == null || s.isEmpty() || s.equalsIgnoreCase("null"));
-    }
-
-    public static String generateId(){
-
-        return UUID.randomUUID().toString();
-    }
-
-    /*public static String generateId(){
-        int leftLimit = 48; // numeral '0'
-        int rightLimit = 122; // letter 'z'
-        int targetStringLength = 8;
-        Random random = new Random();
-
-        String generatedString = random.ints(leftLimit, rightLimit + 1)
-                .filter(i -> (i <= 57 || i >= 65) && (i <= 90 || i >= 97))
-                .limit(targetStringLength)
-                .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
-                .toString();
-
-        return generatedString;*/
 }
