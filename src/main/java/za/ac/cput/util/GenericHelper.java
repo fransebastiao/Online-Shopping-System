1213dfc55392b9ce25e5a72ceaaff455114e4920
Franciel Sebastião <219466912@mycput.ac.za>
2022-04-09 01:51:28 +0200

    Account and Region factory classes created with TDD

--- src/main/java/za/ac/cput/util/GenericHelper.java
+++ src/main/java/za/ac/cput/util/GenericHelper.java
@@ -5,13 +5,14 @@ import java.util.UUID;
 public class GenericHelper {
 
 
-    public static boolean isNullorEmpty(String s){
+    public static boolean isNullorEmpty(String s) {
 
         return (s == null || s.isEmpty() || s.equalsIgnoreCase("null"));
     }
 
-    public static String generateId(){
+    public static String generateId() {
 
         return UUID.randomUUID().toString();
     }
+}
 
