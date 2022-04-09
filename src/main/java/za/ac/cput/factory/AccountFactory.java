dbe6da73d82630b89621b66faa4e7d2243a43a77
Franciel Sebastião <219466912@mycput.ac.za>
2022-04-09 03:24:08 +0200

    Account and Region factory classes created, also TDD for factory classes created

---
+++ src/main/java/za/ac/cput/factory/AccountFactory.java
@@ -0,0 +1,24 @@
+package za.ac.cput.factory;
+import za.ac.cput.entity.Account;
+import za.ac.cput.util.GenericHelper;
+/*
+ Online-Shopping-System
+ Factory for the Account class
+ Author: Franciel Danilo de Carvalho Sebastiao (219466912)
+ Date: 7 April 2022
+*/
+
+public class AccountFactory {
+
+    public static Account saveAccount(String openDate, String closeDate){
+        if(GenericHelper.isNullorEmpty(openDate) || GenericHelper.isNullorEmpty(closeDate))
+            return null;
+
+        String accountId = GenericHelper.generateId();
+
+        return new Account.Builder().setAccountId(accountId)
+                .setOpenDate(openDate)
+                .setCloseDate(closeDate)
+                .build();
+    }
+}
