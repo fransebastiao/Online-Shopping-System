e42f54a974dcbeb18e829e4ca2c61c31b5a1bce2
SipheleleNyathi <siphelelenyathi52@gmail.com>
2022-04-09 17:49:51 +0200

    Removed entity test, added interface and Impl package

---
+++ src/main/java/za/ac/cput/repository/IRepository.java
@@ -0,0 +1,13 @@
+package za.ac.cput.repository;
+/*
+IRepository.java
+Repository interface
+Date: 08 / 04 / 2022
+ */
+
+public interface IRepository<T, ID> {
+    T create(T t);
+    T update(T t);
+    T read(ID id);
+    void delete(ID id);
+}
