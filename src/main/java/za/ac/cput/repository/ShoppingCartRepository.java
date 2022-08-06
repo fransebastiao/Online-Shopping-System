29a6f2974b9bb4b02e07b774fa3a5134ccb04ddc
216217717M <216217717@mycput.ac.za>
2022-08-06 14:40:44 +0200

    Rep added and Domain also

---
+++ src/main/java/za/ac/cput/repository/ShoppingCartRepository.java
@@ -0,0 +1,17 @@
+/*
+ * Online-Shopping-System
+ * Tshepang Molefe
+ * 216217717
+ * 08/Augast/2022
+ */
+package za.ac.cput.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import za.ac.cput.domain.ShoppingCart;
+
+
+@Repository
+public interface ShoppingCartRepository extends JpaRepository<ShoppingCart, String>
+{
+}
