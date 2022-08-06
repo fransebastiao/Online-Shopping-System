f2a497b9dd6faecffc5e4ed139601858e5491483
SipheleleNyathi <siphelelenyathi52@gmail.com>
2022-08-06 12:51:25 +0200

    Fixed Repository error

--- src/main/java/za/ac/cput/repository/UserRepository.java
+++ src/main/java/za/ac/cput/repository/UserRepository.java
@@ -5,11 +5,12 @@ User Repository
 Author: Siphelele Nyathi 218334028
 Date: O4.04.2022
  */
+import org.springframework.data.jpa.repository.JpaRepository;
 import za.ac.cput.domain.User;
 import java.util.Set;
 
 
-public interface UserRepository extends IRepository<User, String>{
+public interface UserRepository extends JpaRepository<User, String> {
 
      Set<User> getAll();
 }
