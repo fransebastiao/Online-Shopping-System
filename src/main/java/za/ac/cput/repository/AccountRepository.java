d2179e731d6a1863564b3db653b81c7bfb9dc52a
Franciel Sebastião <219466912@mycput.ac.za>
2022-08-06 17:57:57 +0200

    Account and Region Repository using JPA framework

--- src/main/java/za/ac/cput/repository/AccountRepository.java
+++ src/main/java/za/ac/cput/repository/AccountRepository.java
@@ -6,9 +6,11 @@ Student Number: 219466912
 Date: O6/08/2022
  */
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import za.ac.cput.domain.Account;
 import java.util.List;
 
+@Repository
 public interface AccountRepository extends JpaRepository<Account, String> {
     List<Account> getAll();
 }
--- src/main/java/za/ac/cput/repository/RegionRepository.java
+++ src/main/java/za/ac/cput/repository/RegionRepository.java
@@ -6,9 +6,11 @@ Student Number: 219466912
 Date: O6/08/2022
  */
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import za.ac.cput.domain.Region;
 import java.util.List;
 
+@Repository
 public interface RegionRepository extends JpaRepository<Region, String>{
     List<Region> getAll();
 }
